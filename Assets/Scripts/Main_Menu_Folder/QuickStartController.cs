4bcf89dcdb294c58dc50243ede0c00fcc473470a
ishumessi10 <ishaang@terpmail.umd.edu>
2025-07-31 12:56:52 -0400

    main menu

---
+++ Assets/Scripts/Main_Menu_Folder/QuickStartController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuickStartController : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+
+    // Called by StartCoOp button
+    public void StartCoOp()
+    {
+        // Load your main game scene, which handles network initialization
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void CloseInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+    }
+}
