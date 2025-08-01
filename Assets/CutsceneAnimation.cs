7e9c2da764b98e857454f4e88ee2476d11fffbbf
jmessin <jmessin2@terpmail.umd.edu>
2025-07-31 21:47:39 -0400

    Cutscene DONE omg

---
+++ Assets/CutsceneAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneAnimationManager : MonoBehaviour
+{
+    public GameObject cutsceneObject; // Reference to the object you want to show/hide
+  //  public float inactiveDuration = 13.0f; // Duration the object will be inactive
+    public float activeDuration = 50.0f; // Duration the object will be active
+
+    void Start()
+    {
+       // cutsceneObject.SetActive(true); // Start inactive
+        StartCoroutine(ShowAndHideObject());
+    }
+
+    IEnumerator ShowAndHideObject()
+    {
+        // object inactive
+      //  yield return new WaitForSeconds(inactiveDuration);
+
+        // activate the object after duration
+        cutsceneObject.SetActive(true);
+
+        yield return new WaitForSeconds(activeDuration); // show on screen for time
+
+        cutsceneObject.SetActive(false); // deactivate
+    }
+}
