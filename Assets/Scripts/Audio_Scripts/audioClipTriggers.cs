8e732c61259ba65e3529cf62587887251bfdaa4e
jmessin <jmessin2@terpmail.umd.edu>
2025-08-01 10:36:39 -0400

    scripts

---
+++ Assets/Scripts/Audio_Scripts/audioClipTriggers.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstEnterSoundDad : MonoBehaviour
+{
+  
+       public AudioSource source; // Assign in Inspector
+         public AudioClip collisionSound; // enter sound
+         public AudioClip leaveCollisionSound; // exit sound
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) //assign player?
+        {
+            source.PlayOneShot(collisionSound);
+        }
+    }
+    void OnCollisionExit (Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            source.PlayOneShot(leaveCollisionSound);
+        }
+    }
+}
